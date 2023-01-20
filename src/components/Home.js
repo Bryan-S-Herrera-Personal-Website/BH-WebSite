855e75e416afa7a945b1f9e02a1d32ecd62bca54
steward <bryanherreraestrada@gmail>
2023-01-20 15:28:21 -0600

    Removed react files and rename files and modify files, added Home component and about component, Now i can start to create the web

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import './app.css'
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Welcome</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+function About() {
+    return(
+    <div className='about'>
+      <header className='About-header'>
+      <h2>About</h2>
+      </header>
+    </div>
+    )
+  }
+
+
+export default About;
