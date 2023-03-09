9450f647cb944674d7efc3570a490d52a12ba698
steward <bryanherreraestrada@gmail>
2023-03-08 19:33:57 -0600

    Changed the about section and added first view of the about secction responsive design

--- src/components/Heroimg3.js
+++ src/components/Heroimg3.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import './css/heroimg3.css'
 import { Component } from 'react'
+import { Link } from 'react-router-dom';
+import MyImg1 from "../assets/MyImg1.jpg"
 
 class HeroImg3 extends Component {
     render() {
       return(  
-        <div className='hero-img3'>
-        <div className='heading3'>
-          <h1>{this.props.heading}</h1>
-          <br></br>
-          <p>Lorem ipsum Restricted Idnt</p>
+        <div className='about'>  
+
+          <div className='main'>
+            <img src={MyImg1}></img>
+            <div className='about-text'>
+              <h1>About Me</h1>
+              <h5>Student of <span>IT Engineer</span></h5>
+              <p>
+              The U.S. is a country of 50 states covering a vast 
+              swath of North America, with Alaska in the northwest
+              and Hawaii extending the nation's presence into the 
+              Pacific Ocean. Major Atlantic Coast cities are New 
+              York, a global finance and culture center, 
+              and capital Washington, DC. Midwestern metropolis 
+              Chicago is known for influential architecture and 
+              on the west coast, Los Angeles' Hollywood is famed 
+              for filmmaking. ― Google
+              </p>
+              <br></br>
+              <Link to="/Contact">
+                 <button className='btn2'>Contact</button>
+              </Link>
+            </div>
+          </div>
+
+        
+
         </div>
-    </div>
     );
    }
   }
