495b4105a37cae21c9e1837fd1615f5f98fb1c00
steward <bryanherreraestrada@gmail>
2023-08-11 18:53:01 -0600

    Added new style of Contact and responsive design for contact page

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,58 +8,71 @@ class HeroImg4 extends Component {
     render() {
       return(
 
+        <div class="contenido">
 
-          <div class="container-contact">
-              <br></br><br></br>
-              
-             <h1>Connect With Me</h1>
-             <p>
-             Don't hesitate to reach out with the contact information below, 
-             or send a message using the form.
-             </p>
-             <div className='contact-box'>
-              <div className='contact-left'>
-                  <h3>Contact Form</h3>
-                  <br></br>
+          
 
-                  <form action="https://formsubmit.co/bryanherreraestrada2@gmail.com" method="POST">
-                  <label>Name</label>
-                  <input type="text" name="name"placeholder="Your Name" required></input>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <hr></hr>
+        <br></br>
 
-                  <label>Email</label>
-                  <input type="email" name="email"placeholder="Your Email" required></input>
+        <h1 class="logo">Contact <span>Me</span></h1>
 
-                  <label>Phone</label>
-                  <input type="text" name="phone" placeholder="Your Phone Number" required></input>
+        <br></br>
+        <br></br>
 
-                  <label>Message</label>
-                  <textarea name="message" rows="4" placeholder='How can I help you?'></textarea>
-                  <br></br>
-                  <button className='btn3' id='Submit-btn' type="submit">Send</button>
-                  </form>
-                  
-              </div>
-              
-              <div className='contact-right'>
-                <h3>Personal Information</h3>
-                <table>
-                  <tr>
-                    <td>Email:</td>
-                    <td>bryanherreraestrada2@gmail.com</td>
-                  </tr>
-                  <tr>
-                    <td>Phone:</td>
-                    <td>506-85557504</td>
-                  </tr>
-                  <tr>
-                    <td>Address:</td>
-                    <td>Esparza, Puntarenas, Costa Rica</td>
-                  </tr>
-                </table>
-              </div>
-             </div>
-             
-          </div>
+        <div class="contact-wrapper animated bounceInUp">
+            <div class="contact-form">
+                <h3>Contact Me</h3>
+                <form action="">
+                    <p>
+                        <label>Name</label>
+                        <input type="text" name="name"></input>
+                    </p>
+                    <p>
+                        <label>Email</label>
+                        <input type="email" name="email"></input>
+                    </p>
+                    <p>
+                        <label>Phone Number</label>
+                        <input type="tel" name="phone"></input>
+                    </p>
+                    <p>
+                        <label>Subject</label>
+                        <input type="text" name="subject"></input>
+                    </p>
+                    <p class="block">
+                       <label>Message</label> 
+                        <textarea name="message" rows="3"></textarea>
+                    </p>
+                    <p class="block">
+                        <button>
+                            Send
+                        </button>
+                    </p>
+                </form>
+            </div>
+            
+
+            <div class="contact-info">
+                <h4>More Info</h4>
+                <ul>
+                    <li><i class="fas fa-map-marker-alt"></i> bryanherreraestrada2@gmail.com</li>
+                    <br></br>
+                    <li><i class="fas fa-phone"></i> (506) 85557504</li>
+                    <br></br>
+                    <li> Esparza, Puntarenas, Costa Rica</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident ipsam necessitatibus repellendus?</p>
+            </div>
+        </div>
+        </div>
+
   );
     
 }
